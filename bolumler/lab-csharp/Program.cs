f79ecbe6838685b83071d3a355a1d8536f38291b
Atilla Ozgur <ati.ozgur@gmail.com>
2024-03-16 10:54:08 +0100

    derleme düzgün

--- bolumler/lab-csharp/Program.cs
+++ bolumler/lab-csharp/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace lab_csharp;
+﻿namespace lab.csharp;
+
+using lab.csharp.UzaktanKumanda;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Console.WriteLine("Hello, World!");
+        UzaktanKumandaSorusu1.Main1();
     }
 }
